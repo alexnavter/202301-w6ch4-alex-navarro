4c7907504adae75de35bc176b8fba34767993e30
Alex Navarro <alexnavter@gmail.com>
2023-02-17 01:05:07 +0100

    Create endpoint error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./loadEnvironment.js";
 import express from "express";
-import thingsRouter from "./routes/router.js";
+import thingsRouter from "./router/router.js";
 
 const app = express();
 
@@ -9,5 +9,9 @@ app.use(express.json());
 const port = process.env.PORT ?? 4003;
 
 app.use("/", thingsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Error. Endpoint not found." });
+});
 
 app.listen(port);
